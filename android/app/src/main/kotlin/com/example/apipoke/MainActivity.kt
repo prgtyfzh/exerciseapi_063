9252b1f973695e415f3b9cf1a8a136ea6afbe536
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2023-05-14 11:44:34 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/apipoke/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apipoke
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/kategori_barang.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../controller/kategori_barang_controller.dart';
+import '../model/kategori_barang_model.dart';
+import 'add_kategori_barang.dart';
+
+class KategoriBarang extends StatefulWidget {
+  const KategoriBarang({super.key});
+
+  @override
+  State<KategoriBarang> createState() => _KategoriBarangState();
+}
+
+class _KategoriBarangState extends State<KategoriBarang> {
+  final kategoriBarangController = KategoriBarangController();
+  List<KategoriBarangModel> listKategoriBarang = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getKategoriBarang();
+  }
+
+  void getKategoriBarang() async {
+    final KategoriBarang = await kategoriBarangController.getKategoriBarang();
+    setState(() {
+      listKategoriBarang = KategoriBarang;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kategori Barang'),
+      ),
+      body: SafeArea(
+          child: ListView.builder(
+        itemCount: listKategoriBarang.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(listKategoriBarang[index].nama),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.edit),
+              ),
+            ),
+          );
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const AddKategoriBarang()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
